a80dd19c2068b98829213fc8915a57ecdbade852
ratis-ai <toolkit-founder@agenticflow.app>
2025-04-06 23:44:03 +0530

    Update licensed file removals

--- pkg/tools/licensed/soc2_auditor/__init__.py
+++
@@ -1,6 +0,0 @@
-from .jira_fetcher import JiraFetcher
-from .gdrive_reader import GDriveReader
-from .policy_checker import PolicyChecker
-from .report_writer import ReportWriter
-
-__all__ = ['JiraFetcher', 'GDriveReader', 'PolicyChecker', 'ReportWriter'] 
